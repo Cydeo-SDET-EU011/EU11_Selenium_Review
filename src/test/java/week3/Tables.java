cc40569ae43b21927a04dcd1627d80bf8355b1f1
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-07-23 19:48:48 +0300

    web table is added.

--- src/test/java/week3/Tables.java
+++ src/test/java/week3/Tables.java
@@ -3,8 +3,10 @@ package week3;
 import io.github.bonigarcia.wdm.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
+import org.testng.*;
 import org.testng.annotations.*;
 
+import java.util.*;
 import java.util.concurrent.*;
 
 public class Tables {
@@ -12,7 +14,7 @@ public class Tables {
     WebDriver driver;
 
     @BeforeMethod
-    public void setup(){
+    public void setup() {
         // 1. Open Chrome browser
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
@@ -20,9 +22,30 @@ public class Tables {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         // 2. Go to https://practice.cydeo.com/registration_form
-        driver.get("https://practice.cydeo.com/nested_frames");
+        driver.get("https://practice.cydeo.com/web-tables");
     }
 
+    @Test
+    public void getName() {
+        String name = driver.findElement(By.xpath("//td[.='Alexandra Gray']")).getText();
+        Assert.assertEquals(name, "Alexandra Gray");
+    }
+
+    @Test
+    public void getNames() {
+        List<WebElement> namesElement = driver.findElements(By.xpath("//tbody//tbody//tr/td[2]"));
+        List<String> names = new ArrayList<>();
+        for (WebElement nameElement : namesElement) {
+            names.add(nameElement.getText());
+        }
+
+        System.out.println(names);
+    }
+
+
+    // create a method, receive two parameters, one name, one pizza count
+    // this method should check if the person ordered correct pizza number
+    //   //td[.='Alexandra Gray']/following-sibling::td[2]
 
 
 }
