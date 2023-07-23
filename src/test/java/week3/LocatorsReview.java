aebcc009ad497a99b0e71bf6a11af197d340988e
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-07-23 16:13:10 +0300

    locators are reviewed.

---
+++ src/test/java/week3/LocatorsReview.java
@@ -0,0 +1,85 @@
+package week3;
+
+import io.github.bonigarcia.wdm.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+
+import java.util.concurrent.*;
+
+public class LocatorsReview {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        // locators
+        // we have 8 locators
+        /*
+        1. id
+        2. name
+        3. class
+        4. tagname
+        5. linktext
+        6. partial link text
+        7. css selector
+        8. xpath
+
+         */
+
+        //id
+        driver.findElement(By.id("content"));
+
+        // name
+        driver.findElement(By.name("viewport"));
+
+        //class
+        driver.findElement(By.className("list-group-item"));
+
+        //tagname
+        driver.findElement(By.tagName("a"));
+
+        //linktext
+        driver.findElement(By.linkText("A/B Testing"));
+
+        //partial link text
+        driver.findElement(By.partialLinkText("Testing"));
+        driver.findElement(By.partialLinkText("A/B Testing"));
+
+        // css selector
+        //    tagname[attribute='value']
+        /*
+        div[class='container']
+
+        we have shortcuts for css selector, we can use
+        # for id
+        . for classname
+
+        div#content
+
+        div.row
+
+        in class value, if we have space, we have to replace space with '.'
+
+        parent child relationship
+        div.col-sm-8 h2    or     div.col-sm-8>h1
+        we can't go from chile --> parent in css
+
+
+        if you have more than 1 child element, you can use nth-of-type(index) method
+        ul.list-group>li:nth-of-type(1)
+        note: index number is starting with 1, not 0.
+
+
+         */
+
+
+        //xpath
+
+
+
+
+    }
+}
