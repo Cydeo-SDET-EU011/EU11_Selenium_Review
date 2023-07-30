337e42b4b2176767b8f17cc8d66854282fd05abb
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-07-30 16:26:36 +0300

    actions is added

--- src/test/java/week4/ActionsTest.java
+++ src/test/java/week4/ActionsTest.java
@@ -2,6 +2,7 @@ package week4;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.*;
+import org.testng.*;
 import org.testng.annotations.*;
 
 public class ActionsTest extends BasePage {
@@ -31,9 +32,44 @@ public class ActionsTest extends BasePage {
         actions.doubleClick(doubleClick).perform();
         Alert alert = driver.switchTo().alert();
         alert.accept();
-
-
     }
+
+    @Test
+    public void dragDropTest() throws InterruptedException {
+        driver.get("https://demos.telerik.com/kendo-ui/dragdrop/index");
+
+        driver.findElement(By.id("onetrust-accept-btn-handler")).click();
+        WebElement source = driver.findElement(By.id("draggable"));
+        WebElement target = driver.findElement(By.id("droptarget"));
+        Actions actions = new Actions(driver);
+        Thread.sleep(3000);
+        actions.dragAndDrop(source,target).perform();
+        String actualText = target.getText();
+        Assert.assertEquals(actualText,"You did great!");
+    }
+
+    @Test
+    public void keyboardActions() throws InterruptedException {
+        driver.get("https://practice.cydeo.com/key_presses");
+        Actions actions = new Actions(driver);
+
+        actions.sendKeys(Keys.SPACE).perform();
+        Thread.sleep(2000);
+        actions.sendKeys(Keys.ENTER).perform();
+        Thread.sleep(2000);
+        actions.sendKeys(Keys.BACK_SPACE).perform();
+        Thread.sleep(2000);
+        actions.sendKeys(Keys.ARROW_DOWN).perform();
+        Thread.sleep(2000);
+        actions.sendKeys(Keys.F1).perform();
+        Thread.sleep(2000);
+        actions.sendKeys(Keys.ESCAPE).perform();
+        Thread.sleep(2000);
+    }
+
+
+
+
 
 }
 
