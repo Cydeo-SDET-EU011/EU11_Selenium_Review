c00a00be7a004095641daea5a8256406bc0c8b49
zulpikarCydeo <102216429+zulpikarCydeo@users.noreply.github.com>
2023-07-30 15:53:02 +0300

    actions is added

--- src/test/java/week4/ActionsTest.java
+++ src/test/java/week4/ActionsTest.java
@@ -4,15 +4,38 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.*;
 import org.testng.annotations.*;
 
-public class ActionsTest extends BasePage{
+public class ActionsTest extends BasePage {
+
+
+
+
+    @BeforeMethod
+    public void setUp() {
+        driver.get("https://chercher.tech/practice/practice-pop-ups-selenium-webdriver");
+
+    }
 
     @Test
-    public void hoverTest(){
-        driver.get("https://chercher.tech/practice/practice-pop-ups-selenium-webdriver");
+    public void hoverTest() {
         WebElement hoverButton = driver.findElement(By.id("sub-menu"));
         Actions actions = new Actions(driver);
         actions.moveToElement(hoverButton).perform();
         driver.findElement(By.id("link2")).click();
 
     }
+
+    @Test
+    public void doubleClickTest() {
+        WebElement doubleClick = driver.findElement(By.id("double-click"));
+        Actions actions = new Actions(driver);
+        actions.doubleClick(doubleClick).perform();
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+
+
+    }
+
 }
+
+
+
